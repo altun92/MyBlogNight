7aac4a32b784f9420d6f633ac9e3bb6e2071c05f
anlal <anlal@ANL>
2024-11-17 23:17:39 +0300

    Add project files.

---
+++ MyBlogNight.PresentationLayer/Program.cs
@@ -0,0 +1,45 @@
+using MyBlogNight.BusinessLayer.Abstract;
+using MyBlogNight.BusinessLayer.Concrete;
+using MyBlogNight.DataAccessLayer.Abstract;
+using MyBlogNight.DataAccessLayer.Context;
+using MyBlogNight.DataAccessLayer.EntityFramework;
+using MyBlogNight.EntityLayer.Concrete;
+using MyBlogNight.PresentationLayer.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddDbContext<BlogContext>();
+builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<BlogContext>().AddErrorDescriber<CustomIdentityErrorValidator>();
+
+builder.Services.AddScoped<IArticleDal, EfArticleDal>();
+builder.Services.AddScoped<IArticleService, ArticleManager>();
+
+builder.Services.AddScoped<ICategoryDal, EfCategoryDal>();
+builder.Services.AddScoped<ICategoryService, CategoryManager>();
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
